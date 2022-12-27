97ac6fc48adad6292f9aa4a97ffc4ed7be9f0bac
Anang Eko Fitriyanto <anangeko9@gmail.com>
2022-12-27 13:59:48 +0700

    latihan golang

---
+++ handler/handler.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+func HalloHandler(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("Selamat datang di perpustakaa Golang"))
+	log.Printf(r.URL.Path)
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(path.Join("views", "index.html"))
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is Happening, Keep Calm", http.StatusInternalServerError)
+		return
+	}
+
+	data := map[string]interface{}{
+		"id":   "1",
+		"nama": "Belajar Golang Fundamental",
+	}
+
+	erro := tmpl.Execute(w, data)
+	if erro != nil {
+		log.Println(err)
+		http.Error(w, "Error is Happening, Keep Calm", http.StatusInternalServerError)
+		return
+	}
+}
+
+func BukuHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	idNumb, err := strconv.Atoi(id)
+
+	if err != nil || idNumb < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Daftar Buku Nomor : %d", idNumb)
+}
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"golangweb/handler"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handler.HalloHandler)
+	mux.HandleFunc("/list", handler.BukuHandler)
+	log.Println(("starting web di port 8080"))
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+
+}
