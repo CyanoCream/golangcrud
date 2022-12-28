30f691eac9f1a3933038aa31bcc3c74970ee0614
Anang Eko Fitriyanto <anangeko9@gmail.com>
2022-12-28 15:23:20 +0700

    kondisi

--- handler/handler.go
+++ handler/handler.go
@@ -32,7 +32,13 @@ func HalloHandler(w http.ResponseWriter, r *http.Request) {
 	// 	"nama": "Belajar Golang Fundamental",
 	// }
 
-	data := entity.Buku{ID: 1, Nama: "Belajar Golang Fundamental", Stock: 3}
+	// data := entity.Buku{ID: 1, Nama: "Belajar Golang Fundamental", Stock: 3}
+
+	data := []entity.Buku{
+		{ID: 1, Nama: "Belajar Golang Fundamental", Stock: 1},
+		{ID: 2, Nama: "SiGolang Si Bocah belajar Golanng", Stock: 3},
+		{ID: 3, Nama: "GoGrak : Go-React", Stock: 2},
+	}
 	erro := tmpl.Execute(w, data)
 	if erro != nil {
 		log.Println(err)
@@ -51,4 +57,34 @@ func BukuHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Daftar Buku Nomor : %d", idNumb)
+}
+
+func ListbukuHandler(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("Selamat datang di perpustakaa Golang"))
+	log.Printf(r.URL.Path)
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(path.Join("views", "index.html"), path.Join("views", "layouts.html"))
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is Happening, Keep Calm", http.StatusInternalServerError)
+		return
+	}
+
+	data := []entity.Buku{
+		{ID: 1, Nama: "Belajar Golang Fundamental", Stock: 4},
+		{ID: 2, Nama: "SiGolang Si Bocah belajar Golanng", Stock: 3},
+		{ID: 3, Nama: "GoGrak : Go-React", Stock: 5},
+	}
+	erro := tmpl.Execute(w, data)
+	if erro != nil {
+		log.Println(err)
+		http.Error(w, "Error is Happening, Keep Calm", http.StatusInternalServerError)
+		return
+	}
 }
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 
 	mux.HandleFunc("/", handler.HalloHandler)
 	mux.HandleFunc("/list", handler.BukuHandler)
+	mux.HandleFunc("/buku", handler.ListbukuHandler)
 	log.Println(("starting web di port 8080"))
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
